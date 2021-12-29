425d55cc85ca602f2eff07c016cb0a0a88f8bf4b
Артем <vip.shark.black@gmail.com>
2021-12-29 13:20:00 +0300

    Доработаны проверки. Pytest пройдены

--- homework.py
+++ homework.py
@@ -1,15 +1,10 @@
-import requests
-import time
+import logging
 import os
+import time
+
+import requests
 import telegram
 from dotenv import load_dotenv
-import logging
-from urllib.error import HTTPError
-from urllib3.exceptions import ConnectTimeoutError
-from requests.exceptions import (
-    ConnectionError, Timeout, RequestException,
-    InvalidHeader, InvalidURL, ProxyError, InvalidProxyURL
-    )
 
 load_dotenv()
 
@@ -17,7 +12,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 1
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -32,52 +27,50 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 
-def send_message(bot, message): #Команда отправки сообщения в Телегу
-    """отправляет сообщение в Telegram чат
-    """
+
+def send_message(bot, message):
+    """Отправляет сообщение в Telegram чат."""
     bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(current_timestamp):
-    """делает запрос к единственному эндпоинту API-сервиса.
-    """
-    timestamp = current_timestamp or int(time.time()) #time.time это текущее время utc. int для округления
+    """Делает запрос к единственному эндпоинту API-сервиса."""
+    timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    result = homework_statuses.json()
+    response = homework_statuses.json()
     if homework_statuses.status_code != 200:
-        raise Exception(f'Статус код не 200! Статус код {homework_statuses.status_code}')
-    return result
-    
+        raise Exception(
+            f'Статус код не 200!'
+            f'Статус код {homework_statuses.status_code}'
+        )
+    return response
 
 
 def check_response(response):
-    """проверяет ответ API на корректность.
-    """
-    homework = response.get('homeworks')
+    """Проверяет ответ API на корректность."""
+    homework = response['homeworks']
     if not homework:
         raise Exception('Нет ключа homeworks')
     if type(homework) is not list:
         raise Exception('домашки приходят не в виде списка в ответ от API')
+    if response is None:
+        raise Exception('Нет ')
+    if not isinstance(response, dict):
+        logging.error('Ошибка работает')
     return homework
 
 
-
-def parse_status(homework): #готово. работает. Проверить путь запроса homework
-    """извлекает из информации о конкретной домашней работе статус этой работы.
-    """
+def parse_status(homework):
+    """Извлечение статуса работы."""
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    try:
-        verdict = HOMEWORK_STATUSES[homework_status]
-    except KeyError as e:
-        logging.error(f'Неверный ключ запроса verdict. Тип ошибки {e}')
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_tokens(): #Проверка работает. Многоточие и None возвращает как некорректный тип данных
-    """проверяет доступность переменных окружения
-    """
+def check_tokens():
+    """Проверяет доступность переменных окружения."""
     try:
         if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
             logging.debug('Проверка переменных окружения завершилась успешно')
@@ -94,36 +87,37 @@ def check_tokens(): #Проверка работает. Многоточие и 
         elif PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
             logging.debug('Проверка переменных окружения завершилась успешно')
             return True
-    except TypeError as e:
-        logging.error('В переменной некорректное значение данных. Тип ошибки {e}')
+    except TypeError as error:
+        logging.error(
+            f'В переменной некорректное значение данных.'
+            f'Тип ошибки {error}')
         return False
-
 
 
 def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 1638138951
-    # int(time.time())
-    check_tokens() #идет проверка переменных, если все ок возвращает значение True и запускается цикл
+    current_timestamp = int(time.time())
+    check_tokens()
     while True:
         try:
-            response = get_api_answer(current_timestamp) # делаем запрос к API домашки
-            check_response(response) # Проверяет ответ API на корректность
-            homework = response.get('homeworks')
-            message = parse_status(homework) #После проверки запроса происходит нарезка данных с запроса
-            
-            current_timestamp = response.get('current_date')
+            response = get_api_answer(current_timestamp)
+            check_response(response)
+            homework = response['homeworks'][0]
+            if homework:
+                logging.info(f'{homework} найден. Данные отправлены')
+                message = parse_status(homework)
+                send_message(bot, message)
+            else:
+                logging.info(f'{homework} не найден. Словарь отсутствует')
+                logging.debug(f'Следующая попытка через {RETRY_TIME} сек')
+            current_timestamp = response['current_date']
             time.sleep(RETRY_TIME)
-
         except Exception as error:
-            message = f'Сбой в работе программы: {error}' # Это перечень ошибок в котороых переменные доступны, но что-то пошло не так
+            message = f'Сбой в работе программы: {error}'
+            logging.error(f'Проблема с работой. Ошибка{error}')
             send_message(bot, message)
             time.sleep(RETRY_TIME)
-        else:
-            message = 'Проблема с глобальными переменными и токен. Проверка на наличие не пройдена.'
-            send_message(bot, message)
-            raise SystemExit
 
 
 if __name__ == '__main__':
--- tt.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def check_response(response):
-    """проверяет ответ API на корректность.
-    """
-    try:
-        homeworks = response.get('homeworks')
-    except None as e:
-        return f'Список работ пуст. Тип ошибки {e}.'
